d9187e13092a0f95642cc8950f29a7708ab79a40
tl5866 <33259925+tl5866@users.noreply.github.com>
2025-04-12 19:11:24 +0200

    api key now in settings

--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
